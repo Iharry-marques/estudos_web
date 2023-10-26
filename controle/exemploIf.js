5f049599ec03921f6f0187b6aa0cbde569f79690
fmmoliveira <fabio.marques.melo@gmail.com>
2023-10-26 12:54:01 -0300

    newCommit

---
+++ controle/exemploIf.js
@@ -0,0 +1,10 @@
+function notaFinal (nota){
+    if (nota > 7 ){
+        console.log("Aprovado com " + nota)
+    } else{
+        console.log("Reprovado com " + nota)
+    }
+}
+
+notaFinal(7.1)
+notaFinal(6.1)
---
+++ controle/ifElse.js
@@ -0,0 +1,12 @@
+const imprimirResultado = function (nota){
+    if(nota >=7){
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(8)
+imprimirResultado(6)
+imprimirResultado('Epa !')
+imprimirResultado('10')
